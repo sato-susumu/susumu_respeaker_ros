6f38afaa34aaeb7de03e9e8237ee940f2fe14553
sato-susumu <75652942+sato-susumu@users.noreply.github.com>
2025-01-25 13:25:48 +0900

    bug fix

--- setup.py
+++ setup.py
@@ -1,34 +1,43 @@
-from setuptools import find_packages
-from setuptools import setup
+import os
+from glob import glob
+from setuptools import find_packages, setup
 
-package_name = 'respeaker_ros'
+package_name = "susumu_respeaker_ros"
+
 setup(
     name=package_name,
-    version='1.0.0',
+    version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/' + package_name, ['package.xml']),
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name])],
-    py_modules=[],
-    install_requires=['setuptools'],
-    author='Nick Walker',
-    author_email='nswalker@cs.washington.edu',
-    maintainer='Nick Walker',
-    maintainer_email='nswalker@cs.washington.edu',
-    keywords=['ROS'],
-    classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python',
-        'Topic :: Software Development',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+
+        # launchファイルをインストール対象に含める (launch/*.launch.py など)
+        (os.path.join('share', package_name, 'launch'),
+            glob(os.path.join('launch', '*.launch.*'))),
     ],
-    description='ROS interface for respeaker.',
+    install_requires=[
+        'setuptools',
+        # 以下、pipでインストールさせたいPythonライブラリ
+        'pyaudio',
+        'numpy',
+        'torch',
+        'vosk',
+        'google-cloud-speech',
+        'openwakeword',
+    ],
+    zip_safe=True,
+    maintainer='Sato Susumu',
+    maintainer_email='75652942+sato-susumu@users.noreply.github.com',
+    description='TODO: Package description',
     license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'respeaker_node = respeaker_ros.respeaker_node:main',
+            # ROS 2 ノードとして起動する際に呼び出せるエントリポイントを指定
+            # ここで指定した名前を、launchファイル等から `executable=` に使うことが多い
+            'susumu_asr_node = susumu_asr_ros.susumu_asr_node:main',
         ],
     },
 )
